61c127591dfc69aff9c9f0aa96688bff24109c40
eyupGuc <meydanguresi@gmail.com>
2022-11-17 06:47:32 +0300

    first commit

---
+++ src/app-router/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { Dashboard } from "@mui/icons-material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+
+const AppRouter = () => {
+  return (
+    <h2>Hello</h2>
+  );
+};
+
+export default AppRouter;
